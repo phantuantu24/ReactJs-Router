87ca21025b61086c6d3e317494a5b3093e78f7da
TuPT <nhokkon2497@gmail.com>
2021-03-12 16:40:38 +0700

    custom with Link

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
 import './App.css';
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Contact from './components/Contact/Contact';
 
 function App() {
+
+  function MyCustomNavLink({ label, to, activeOnlyWhenExact }) {
+    return (
+      <Route path={to} exact={activeOnlyWhenExact} children={({match}) => {
+        const active = match ? 'active' : ''
+        return (
+          <li className={active}>
+            <Link to={to} className="my-Link">{label}</Link>
+          </li>
+        )
+      }} />
+    )
+  }
+
   return (
     <Router>
       <div className="App">
         {/* Menu */}
-        <nav className="navbar navbar-inverse">
+        <nav className="navbar navbar-default">
           <ul className="nav navbar-nav">
-            {/* <li>
-              <NavLink activeStyle={{
-                backgroundColor: 'white',
-                color: 'red'
-              }} exact="true" to="/" className="my-NavLink">Home</NavLink>
-            </li> */}
-            <li>
-              <NavLink activeClassName="active" exact to="/" className="my-NavLink">Home</NavLink>
-            </li>
-            <li>
-              <NavLink activeClassName="active" to="/contact" className="my-NavLink">Contact</NavLink>
-            </li>
-            <li>
-              <NavLink activeClassName="active" to="/about" className="my-NavLink">About</NavLink>
-            </li>
+            <MyCustomNavLink label="Home" to="/" activeOnlyWhenExact={true}/>
+            <MyCustomNavLink label="Contact" to="/contact" activeOnlyWhenExact={false}/>
+            <MyCustomNavLink label="About" to="/about" activeOnlyWhenExact={false}/>
           </ul>
         </nav>
         {/* Content */}
-        <Route path="/" exact component={Home}/>
-        <Route path="/about" component={About}/>
-        <Route path="/contact" component={Contact}/> 
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
       </div>
     </Router>
   );
