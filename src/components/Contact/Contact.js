fd84402b7a42669b8aed530da967aa491478a6d6
TuPT <nhokkon2497@gmail.com>
2021-03-12 14:54:26 +0700

    using Link to apply single-page

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Contact.css';
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h2>This is Contact Page</h2>
+      </div>
+    );
+  }
+}
+
+export default Contact;
