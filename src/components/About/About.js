474142d94da0f41ce5c2f51cc2aef312509da46b
TuPT <nhokkon2497@gmail.com>
2021-03-12 14:43:09 +0700

    using router and route for bind a url with component

---
+++ src/components/About/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './About.css';
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h2>This is About Page</h2>
+      </div>
+    );
+  }
+}
+
+export default About;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>This is Home Page</h2>
+      </div>
+    );
+  }
+}
+
+export default Home;
